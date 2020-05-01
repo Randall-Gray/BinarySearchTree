7a93e9e8d5b9e5291df19e3e37bb41616c734883
Randall Gray <randall.gray7260@gmail.com>
2020-05-01 13:05:34 -0500

    Initial code

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node<T> where T : IComparable
+    {
+        // Member variables
+        T data;
+
+        Node<T> leftChild;
+        Node<T> rightChild;
+    }
+}
